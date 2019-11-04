384fcfe4858b79492e25c6ae9494bc97c21cea6b
Whittney Davis <wcdavis89@yahoo.com>
2019-11-04 12:55:22 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -126,5 +126,5 @@ def adding_to_dennis
         }
      }
      denniss_new_info="Assembly"
-     programmer_hash[:dennis_ritchie][:languages]. denniss_new_info
+     programmer_hash[:dennis_ritchie][:languages].push denniss_new_info
      end
