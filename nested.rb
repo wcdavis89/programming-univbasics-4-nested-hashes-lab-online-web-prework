8b3aeb46d4805d1bf8fa609eaff8038ae1e43257
Whittney Davis <wcdavis89@yahoo.com>
2019-11-04 12:56:50 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -125,5 +125,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-     programmer_hash[:dennis_ritchie][:languages].push(denniss_new_info)
+     programmer_hash[:dennis_ritchie][:languages].push("Assembly")
      end
